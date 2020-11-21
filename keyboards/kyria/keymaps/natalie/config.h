0282ecdf5966c29db1138ff3e0d780438426576c
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2020-11-21 09:05:53 -0700

    Switch to mod-taps on home row (excepting shift)

--- keyboards/kyria/keymaps/natalie/config.h
+++ keyboards/kyria/keymaps/natalie/config.h
@@ -16,6 +16,15 @@
 
 #pragma once
 
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Recommended for heavy chording
+#define QMK_KEYS_PER_SCAN 4
+
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
 #endif
