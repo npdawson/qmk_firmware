bb775d1aeff2fc58b19970448206687ab4bb1491
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2022-02-26 14:46:01 -0700

    Work towards 'arpeggio' style layout

--- keyboards/kyria/keymaps/natalie/rules.mk
+++ keyboards/kyria/keymaps/natalie/rules.mk
@@ -1,6 +1,6 @@
 OLED_DRIVER_ENABLE = yes  # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 LEADER_ENABLE = no         # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = no
