5d5eae73951015ef4b0075dc86fbeb968b7f20c2
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2023-01-07 08:53:39 -0700

    Initial foray into userspace keymaps

---
+++ keyboards/splitkb/kyria/keymaps/natalieHD/rules.mk
@@ -0,0 +1,8 @@
+OLED_ENABLE = yes  # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+LEADER_ENABLE = no         # Enable the Leader Key feature
+MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = no
+
+USER_NAME := ericgebhart
