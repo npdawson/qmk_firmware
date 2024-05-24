41d4577fc0daaf759112251852655cecd2ff68af
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2024-05-24 15:09:29 -0700

    fix function layer

--- keyboards/sofle/keymaps/natalie/keymap.c
+++ keyboards/sofle/keymaps/natalie/keymap.c
@@ -313,9 +313,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|
        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PSCR,
     //|--------+--------+--------+--------+--------+--------|
-       XXXXXXX, KC_F3,   KC_F3,   KC_F3,   KC_F8,   KC_SCRL,
+       XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_SCRL,
     //|--------+--------+--------+--------+--------+--------|  ===  |
-       XXXXXXX, KC_F3,   KC_F10,  KC_F11,  KC_F12,  KC_PAUS, XXXXXXX,
+       XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PAUS, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|  ===  |
                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //                  \--------+--------+--------+--------+-------/
